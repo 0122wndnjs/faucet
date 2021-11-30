85c872897f5e89d464490940f44629654eebe9ae
Joowon Kim <0122wndnjs@gmail.com>
2021-11-30 16:56:42 +0900

    modifying contracts

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,32 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint public funds = 1000; // positive values only
-    int public counter = -10;
-    uint32 public test = 4294967295;
+
+    // this is a special function
+    // it's called when you make a transaction that doesn't specify 
+    // function name to call
+
+    // External function is part of the contract interface
+    // which means they can be called via contracts and other transactions
+
+    receive() external payable {}
+    function addFunds() external payable {}
+
+    function justTesting() external pure returns(uint) {
+        return 2 + 2;
+    }
+
+    // pure, view - read-only call, no gas fee
+    // view - it indicates that the function will not alter the storage state in any way
+    // pure - even more strict, indicating that it won't even read the storage state
+
+    // Transaction (can generate state changes) and require gas fee
+    // read-only call, no gas fee
+
+    // to talk to the node on the network you can make JSON-RPC http calls
 }
+
+// Block info
+// Nonce - a hash that when combined with the minHash proofs that
+// the block has gone through proof of work(POW)
+// 8 bytes => 64 bits
