e588b0362f7c05734ba2a7bdb1fe951108bc82f6
Joowon Kim <0122wndnjs@gmail.com>
2021-12-07 17:04:55 +0900

    Transaction example added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,202 @@
+import React, { Component }  from 'react';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from "./utils/load-contract";
+
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    isProviderLoaded: false,
+    web3: null,
+    contract: null
+  })
+
+  const [balance, setBallance] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [shouldReload, reload] = useState(false)
+
+  const canConnectToContract = account && web3Api.contract
+  const reloadEffect = useCallback(() => reload(!shouldReload), [shouldReload])
+
+  const setAccountListener = provider => {
+    provider.on("accountsChanged", _ => window.location.reload())
+    provider.on("chainChanged", _ => window.location.reload())
+  }
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+
+      if (provider) {
+        const contract = await loadContract("Faucet", provider)
+        setAccountListener(provider)
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+          contract,
+          isProviderLoaded: true
+        })
+      } else {
+        setWeb3Api(api => ({...api, isProviderLoaded: true}))
+        console.error("Please, install Metamask.")
+
+      // if (window.ethereum) {
+      //   provider = window.ethereum;
+
+      //   try {
+      //     await provider.request({method: "eth_requestAccounts"});
+      //   } catch {
+      //     console.error("User denied accounts access!")
+      //   }
+      // } else if (window.web3) {
+      //   provider = window.web3.currentProvider
+      // } else if (!process.env.production) {
+      //   provider = new Web3.providers.HttpProvider("http://localhost:7545")
+      // }
+
+      // setWeb3Api({
+      //   web3: new Web3(provider),
+      //   provider
+      // })
+
+      // with metamask we haev an access to window.ethereum & to window.web3
+      // metamask injects a global API into website
+      // this API allows websites to request users, accounts, read data to blockchain,
+      // sign messages and transactions
+
+      // console.log(window.web3)
+      // console.log(window.ethereum)
+      }
+    }
+
+    loadProvider()
+  }, [])
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBallance(web3.utils.fromWei(balance, "ether"))
+    }
+
+    web3Api.contract && loadBalance()
+  }, [web3Api, shouldReload])
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether")
+    })
+
+    reloadEffect()
+    // window.location.reload()
+  }, [web3Api, account, reloadEffect])
+
+  // const addFunds = async() => {
+  //   const { contract, web3 } = web3Api
+  //   await contract.addFunds({
+  //     from: account,
+  //     value: web3.utils.toWei("1", "ether")
+  //   })
+  // }
+
+  const withdraw = async () => {
+    const { contract, web3 } = web3Api
+    const withdrawAmount = web3.utils.toWei("0.1", "ether")
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+    reloadEffect()
+  }
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          { web3Api.isProviderLoaded ?
+            <div className="is-flex is-align-items-center">
+              <span>
+                <strong className="mr-2">Account: </strong>
+              </span>
+                { account ?
+                  <div>{account}</div> :
+                  !web3Api.provider ?
+                  <>
+                    <div className="notification is-warning is-size-6 is-rounded">
+                      Wallet is not detected!{` `}
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://docs.metamask.io">
+                        Install Metamask
+                      </a>
+                    </div>
+                  </> :
+                  <button
+                    className="button is-small"
+                    onClick={() =>
+                      web3Api.provider.request({method: "eth_requestAccounts"}
+                    )}
+                  >
+                    Connect Wallet
+                  </button>
+                }
+            </div> :
+            <span>Looking for Web3...</span>
+          }
+          <div className="balance-view is-size-2 my-4">
+            Current Balance: <strong>{balance}</strong> ETH
+          </div>
+          { !canConnectToContract &&
+            <i className="is-block">
+              Connect to Ganache
+            </i>
+          }
+          {/* <button 
+            className="btn mr-2"
+            onClick={async () => {
+              const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+              console.log(accounts)
+            }}
+          >
+            Enable Ethereum
+          </button> */}
+          <button
+            disabled={!canConnectToContract}
+            onClick={addFunds}
+            className="button is-link mr-2">
+              Donate 1 eth
+            </button>
+          <button
+            disabled={!canConnectToContract}
+            onClick={withdraw}
+            className="button is-primary">Withdraw 0.1 eth</button>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+// private key 32 byte number
+// 304b0740b89eb62b0d9556b46c05dd25ae1e911852e9a1da8c5d396c575f6083
+
+// public key(uncompressed) 64 byte number
+// 04b1aec3388ad4b7c1dd9be23744488b43ee008aa9280b2aaf71c07d02962f140ac56768cdd5a942a85beb9352cd857de8c6cbeee60d213b1c4ab988851b0a413e
+
+// public key(compressed) 33 byte number
+// 02b1aec3388ad4b7c1dd9be23744488b43ee008aa9280b2aaf71c07d02962f140a
